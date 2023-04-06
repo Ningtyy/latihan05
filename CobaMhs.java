c13a794e49cb266b20acc23727eb43cc1170506e
Ningtyy <129071512+Ningtyy@users.noreply.github.com>
2023-04-06 20:00:04 +0700

    Add files via upload

---
+++ CobaMhs.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Bening Tyas
+ */
+public class CobaMhs {
+    public static void main(String[] args){
+        Mhs datamhs = new Mhs();
+        
+        datamhs.setNama ("Arya Widyanto");
+        System.out.print ("Nama Mhs : " + datamhs.getNama());
+    }
+}
---
+++ Mhs.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Bening Tyas
+ */
+public class Mhs {
+    //property..
+    private String nama; 
+    private float ipk;
+    
+    //behavior...
+    public void setNama (String nama){
+        this.nama = nama;
+    }
+    public void setIpk (int ipk) {
+        this.ipk = ipk;
+    }
+    public String getNama (){
+        return this.nama;
+    }
+}
